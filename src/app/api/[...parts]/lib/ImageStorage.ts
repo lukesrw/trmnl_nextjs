34893254416acb357b94f52547f9c82f1c44cf8a
lukesrw <lukesrw+github@gmail.com>
2025-02-13 17:33:25 +0000

    Updated to have directory path

--- src/app/api/[...parts]/lib/ImageStorage.ts
+++ src/app/api/[...parts]/lib/ImageStorage.ts
@@ -4,15 +4,17 @@ import { TrmnlRequest } from "../../lib/TrmnlRequest";
 
 export class ImageStorage {
     url: string;
+    directory: string;
     path: string;
 
     constructor(trmlRequest: TrmnlRequest, public fileName: string) {
         if (trmlRequest.isServerless) {
             this.url = `http://${trmlRequest.host}/api/display-tmp?image=${fileName}`;
-            this.path = join(tmpdir(), "trmnl", fileName);
+            this.directory = join(tmpdir(), "trmnl");
         } else {
             this.url = `http://${trmlRequest.host}/img/${fileName}`;
-            this.path = join(process.cwd(), "public", "img", fileName);
+            this.directory = join(process.cwd(), "public", "img");
         }
+        this.path = join(this.directory, fileName);
     }
 }
