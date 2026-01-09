b6fcd77191e7e4660b0f38392f92b28a4ce1f23c
lukesrw <lukesrw+github@gmail.com>
2026-01-09 16:44:29 +0000

    Add variables

--- src/app/api/dashboard/route.tsx
+++ src/app/api/dashboard/route.tsx
@@ -4,7 +4,7 @@ import { getScreen } from "./getScreen";
 export async function POST(request: NextRequest) {
     const data = await request.json();
 
-    const image = await getScreen(data.config, data.mode);
+    const image = await getScreen(screen.width, screen.height, data.config, data.mode);
 
     return new Response(Buffer.from(image));
 }
